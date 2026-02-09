1c4c8f559a64fe57c68d6f7113b76114cb8ebe55
free_Domek <dominikschmied4@gmail.com>
2026-02-09 04:54:04 +0100

    Add files via upload

---
+++ 06_SHARED/hooks/useBarbieriiClient.ts
@@ -0,0 +1,6 @@
+/**
+ * Wrapper hook pro Barbieri API client
+ * Re-export z Reality vrstvy pro použití v UI
+ */
+export { useBarbieriiClient } from '@reality/barbieri-api/client';
+export type { BarbieriiTelemetry, ConnectionState } from '@reality/barbieri-api/client';
