dca7b2e8ce7d5758924f4a1d62d53b48e75b3954
free_Domek <dominikschmied4@gmail.com>
2026-02-10 11:36:26 +0100

    Create cn utility wrapper in shared layer

---
+++ 06_SHARED/utils/cn.ts
@@ -0,0 +1,10 @@
+/**
+ * Class name utility (cn)
+ * Combines clsx and tailwind-merge for optimal className handling
+ */
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
