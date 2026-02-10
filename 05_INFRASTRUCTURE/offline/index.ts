cca710706a62eeb98796c13fb15ab9da734ad1b2
free_Domek <dominikschmied4@gmail.com>
2026-02-10 09:34:50 +0100

    Create offline layer index for centralized exports

---
+++ 05_INFRASTRUCTURE/offline/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Infrastructure Layer - Offline Support
+ * Centrální export pro offline funkcionalitu
+ */
+
+export { saveMthUpdate, processOfflineQueue } from './offline-queue';
+export type { OfflineQueueItem } from './offline-queue';
