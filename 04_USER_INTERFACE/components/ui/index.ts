56752e4bdf3002e47745fc3ee188214e7b921c04
free_Domek <dominikschmied4@gmail.com>
2026-02-10 11:36:48 +0100

    Create UI components index for centralized exports

---
+++ 04_USER_INTERFACE/components/ui/index.ts
@@ -0,0 +1,23 @@
+/**
+ * UI Layer - Components Index
+ * Centrální export pro všechny UI komponenty
+ */
+
+// shadcn/ui components
+export * from './button';
+export * from './card';
+export * from './input';
+export * from './label';
+export * from './select';
+export * from './textarea';
+export * from './dialog';
+export * from './dropdown-menu';
+export * from './alert-dialog';
+export * from './badge';
+export * from './checkbox';
+export * from './tooltip';
+export * from './skeleton';
+export * from './collapsible';
+export * from './toaster';
+export * from './sonner';
+export * from './use-toast';
