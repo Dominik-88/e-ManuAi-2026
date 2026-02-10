23b1688289e72cee0e8f52396cfb12078759e507
free_Domek <dominikschmied4@gmail.com>
2026-02-10 09:34:48 +0100

    Create auth layer index for centralized exports

---
+++ 05_INFRASTRUCTURE/auth/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Infrastructure Layer - Authentication
+ * Centrální export pro autentizaci
+ */
+
+export { AuthProvider, useAuth } from './AuthContext';
+export { ProtectedRoute } from './ProtectedRoute';
