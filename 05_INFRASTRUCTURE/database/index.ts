3a149b9eeac7e1cf108069afd8805611d2493724
free_Domek <dominikschmied4@gmail.com>
2026-02-10 09:34:48 +0100

    Create database layer index for centralized exports

---
+++ 05_INFRASTRUCTURE/database/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Infrastructure Layer - Database
+ * Centrální export pro databázové typy a klienta
+ */
+
+// Supabase client
+export { supabase } from './types/client';
+
+// Database types
+export type { Database } from './types/types';
+export type * from './types/database';
