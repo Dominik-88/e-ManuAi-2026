d9f4ec6b4bfec2e431d7c972af03aa0bb64385cb
free_Domek <dominikschmied4@gmail.com>
2026-02-10 11:36:41 +0100

    Create shared hooks index for centralized exports

---
+++ 06_SHARED/hooks/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Shared Layer - Hooks
+ * Centrální export pro všechny hooks
+ */
+
+export { useMachine } from './useMachine';
+export { useBarbieriiClient } from './useBarbieriiClient';
+export { useOfflineSync } from './useOfflineSync';
+export { useOnlineStatus } from './useOnlineStatus';
+export { useSunGlare } from './useSunGlare';
+export { useToast, toast } from './use-toast';
+export { useIsMobile } from './use-mobile';
